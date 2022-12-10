ff5bd58012113cc62267139ae10395db580c9b9e
1t-n00b <mummintrol@gmail.com>
2022-12-10 18:00:08 +0300

    refactor

--- src/App.tsx
+++ src/App.tsx
@@ -12,22 +12,13 @@ import CircularProgress from '@mui/material/CircularProgress';
 function App() {
     console.log('APP');
     const dispatch = useAppDispatch();
-    // let toDoLists = useAppSelector(state => state.toDoLists);
-    // const isLoading = useAppSelector(state => state.app.status);
+
     const isInitialized = useAppSelector(state => state.app.isInitialized);
     const error = useAppSelector(state => state.app.error);
     useEffect(() => {
         dispatch(initializeAppTC());
     }, [dispatch]);
-    // const dispatch = useAppDispatch();
-    //
-    // useEffect(() => {
-    //     dispatch(fetchToDoListsTC());
-    // }, [dispatch]);
 
-    // const addNewToDoList = useCallback((title: string) => {
-    //     dispatch(addNewToDoListTC(title));
-    // }, [dispatch]);
     if (!isInitialized) {
         return <div
             style={{position: 'fixed', top: '30%', textAlign: 'center', width: '100%'}}>
@@ -45,27 +36,6 @@ function App() {
                 <Route path="/404" element={<h1>404</h1>}/>
                 <Route path="*" element={<Navigate to="/404"/>}/>
             </Routes>
-
-
-
-            {/*<div className="App">*/}
-
-            {/*<AddItemForm addItem={addNewToDoList}/>*/}
-            {/*<Grid container spacing={3}>*/}
-            {/*    {toDoLists.map((t) => {*/}
-            {/*        return <Grid item key={t.id}>*/}
-            {/*            <Paper className={'todoPaper'} elevation={8}*/}
-            {/*                   style={{padding: '10px', backgroundColor: 'InfoBackground'}*/}
-            {/*                   }>*/}
-            {/*                <ToDoList toDoList={t} key={t.id} toDoListID={t.id}/>*/}
-            {/*            </Paper>*/}
-            {/*        </Grid>;*/}
-            {/*    })}*/}
-            {/*</Grid>*/}
-            {/*<ToDoLISTS/>*/}
-
-            {/*</div>*/}
-
         </div>
 
     );
--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -27,7 +27,7 @@ export const Login = () => {
     const formik = useFormik({
         initialValues: {
             email: '',
-            password: '3WkYr_gjnmmQ5AE',
+            password: '',
             rememberMe: true
         },
         validate: values => {
